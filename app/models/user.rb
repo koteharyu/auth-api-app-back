7c43a4453ddf7afebfaa696d7dbc580e3a0462c5
haryu <hryau6@gmail.com>
2021-10-16 13:14:36 +0900

    add: validations of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
